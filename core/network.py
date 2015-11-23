70f000bb8cee64676a40bb96b7e7140ddf9fa718
clint <clint@clintui-MacBook-Pro.local>
2015-11-24 02:35:43 +0900

    change activation function

--- core/network.py
+++ core/network.py
@@ -34,7 +34,7 @@ class Network:
         '''
         >>> np.random.seed(0xC0FFEE)
         >>> n = Network()
-        >>> n.layers.append( Layer(2, 10, Tanh.function, Tanh.derivative) )
+        >>> n.layers.append( Layer(2, 10, ReLu.function, ReLu.derivative) )
         >>> n.layers.append( Layer(10, 2) )
         >>> for epoch in range(0, 20):
         ...     loss = n.train( x = np.array([ [1, 2, 1, 2,  5, 6, 5, 6],
@@ -43,10 +43,10 @@ class Network:
         ...                                    [0, 0, 0, 0,  1, 1, 1, 1]]) )
         ...     if epoch%5 == 0:
         ...         print 'epoch:%04d loss:%.2f'%(epoch, loss)
-        epoch:0000 loss:2.95
-        epoch:0005 loss:0.52
-        epoch:0010 loss:0.33
-        epoch:0015 loss:0.24
+        epoch:0000 loss:4.92
+        epoch:0005 loss:0.19
+        epoch:0010 loss:0.12
+        epoch:0015 loss:0.09
         >>> y = n.predict( np.array( [[1, 6, 3], [5, 1, 4]] ) )
         >>> [_ for _ in np.argmax(y, 0)]
         [0, 1, 0]
