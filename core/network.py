e83fb055b461752fee93934c45d361283edca593
clint <cilnt.baek@daumkakao.com>
2015-11-26 11:04:20 +0900

    add not update updater

--- core/network.py
+++ core/network.py
@@ -36,6 +36,7 @@ class Network(object):
 
     def __test(self):
         '''
+        >>> # Multiclass classification
         >>> from layers import Fullconnect
         >>> from nonlinears import ReLu, Tanh
         >>> from activations import Softmax, Sigmoid, Identity
@@ -59,6 +60,8 @@ class Network(object):
         >>> y = n.predict( np.array( [[1, 6, 3], [5, 1, 4]] ) )
         >>> [_ for _ in np.argmax(y, 0)]
         [0, 1, 0]
+        >>>
+        >>> # Multiple-class classification
         >>> n = Network()
         >>> n.layers.append( Fullconnect(2, 10, ReLu.function, ReLu.derivative, updater=GradientDescent(learning_rate=0.01)) )
         >>> n.layers.append( Fullconnect(10, 2, updater=GradientDescent(learning_rate=0.01)) )
@@ -80,6 +83,8 @@ class Network(object):
         >>> y = n.predict( np.array( [[1, 6, 3], [5, 1, 4]] ) )
         >>> [_ for _ in np.argmax(y, 0)]
         [0, 1, 0]
+        >>>
+        >>> # Regression
         >>> n = Network()
         >>> n.layers.append( Fullconnect(2, 10, ReLu.function, ReLu.derivative, updater=GradientDescent(learning_rate=0.01)) )
         >>> n.layers.append( Fullconnect(10, 2, updater=GradientDescent(learning_rate=0.01)) )
@@ -98,6 +103,35 @@ class Network(object):
         >>> y = n.predict( np.array( [[1, 6, 3, 5], [5, 1, 4, 5]] ) )
         >>> [['%.2f'%_ for _ in v] for v in y]
         [['1.36', '0.43', '0.72', '0.54'], ['0.15', '0.69', '0.52', '0.63']]
+        >>>
+        >>> # Auto-encoder
+        >>> from core.updaters import NotUpdate
+        >>> n = Network()
+        >>> n.layers.append( Fullconnect( 2, 10, updater=GradientDescent(learning_rate=0.001)) )
+        >>> n.layers.append( Fullconnect(10, 10, Tanh.function, Tanh.derivative, GradientDescent(learning_rate=0.001)) )
+        >>> n.layers.append( Fullconnect(10, 10, updater=NotUpdate()) )
+        >>> n.layers.append( Fullconnect(10,  2, updater=NotUpdate()) )
+        >>> n.activation = Identity()
+        >>> # for auto-encoder (weight share)
+        >>> n.layers[2].W = n.layers[1].W.T
+        >>> n.layers[3].W = n.layers[0].W.T
+        >>> x = np.array( [[1, 2, 1, 2,  5, 6, 5, 6,  5, 6, 5, 6],
+        ...                [5, 4, 4, 5,  5, 4, 5, 4,  1, 2, 2, 1]] )
+        >>> for epoch in range(0, 1001):
+        ...     loss = n.train( x=x, target=x )
+        ...     if epoch%100 == 0:
+        ...         print 'epoch:%04d loss:%.2f'%(epoch, loss)
+        epoch:0000 loss:53.29
+        epoch:0100 loss:13.46
+        epoch:0200 loss:10.27
+        epoch:0300 loss:8.39
+        epoch:0400 loss:7.99
+        epoch:0500 loss:8.04
+        epoch:0600 loss:8.34
+        epoch:0700 loss:8.45
+        epoch:0800 loss:8.17
+        epoch:0900 loss:8.03
+        epoch:1000 loss:7.97
         '''
         pass
 
--- core/updaters/__init__.py
+++ core/updaters/__init__.py
@@ -1,1 +1,2 @@
 from gradient_descent import GradientDescent
+from not_update import NotUpdate
