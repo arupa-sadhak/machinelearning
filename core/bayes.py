ccac63f310226c6ab36267017dc9c1ae9dc0af51
wbaek <wbaek.personal@gmail.com>
2016-03-25 16:28:41 +0900

    refactoring generalize bayes classifier

--- core/bayes.py
+++ core/bayes.py
@@ -13,8 +13,8 @@ class NaivBayes(object):
                 self.num_classes, str(self.priors), str(self.means), str(self.covs) )
 
     def predict(self, x):
-        _ = np.array( [self.priors[i] * multivariate_normal.pdf(x, mean=m, cov=np.diag(s), allow_singular=True)
-            for i, (m, s) in enumerate(zip(self.means, self.covs))] )
+        _ = np.array( [self.priors[i] * multivariate_normal.pdf(x, mean=m, cov=c, allow_singular=True)
+            for i, (m, c) in enumerate(zip(self.means, self.covs))] )
         norm = np.sum(_, axis=0)
         _ = np.divide(_, norm)
         return _.T
@@ -22,7 +22,7 @@ class NaivBayes(object):
     def train(self, x, target):
         self.priors = [sum([1.0 for t in target if t==k])/len(target) for k in range(self.num_classes)]
         self.means = [ np.mean( [v for v, t in zip(x, target) if t==k], axis=0 ) for k in range(self.num_classes) ]
-        self.covs = [ np.var( [v for v, t in zip(x, target) if t==k], axis=0 ) for k in range(self.num_classes) ]
+        self.covs = [ np.diag( np.var( [v for v, t in zip(x, target) if t==k], axis=0 ) ) for k in range(self.num_classes) ]
 
     def dump_params(self):
         odict = {}
@@ -46,7 +46,7 @@ class NaivBayes(object):
         ...                         [5, 4,  1, 2, 2, 1, 1, 2]]).T,
         ...     target = np.array(  [0, 0,  1, 1, 1, 1, 1, 1] ) )
         >>> str(c)
-        'num_classes:2, priors:[0.25, 0.75], means:[array([ 1.5,  4.5]), array([ 6. ,  1.5])], covs:[array([ 0.25,  0.25]), array([ 1.  ,  0.25])]'
+        'num_classes:2, priors:[0.25, 0.75], means:[array([ 1.5,  4.5]), array([ 6. ,  1.5])], covs:[array([[ 0.25,  0.  ],\\n       [ 0.  ,  0.25]]), array([[ 1.  ,  0.  ],\\n       [ 0.  ,  0.25]])]'
         >>> y = c.predict( np.array( [[1, 6, 3],
         ...                           [5, 1, 4]] ).T )
         >>> print ['%.2f'%_ for _ in y[0]]
@@ -57,13 +57,6 @@ class NaivBayes(object):
         pass
 
 class GeneralBayes(NaivBayes):
-    def predict(self, x):
-        _ = np.array( [self.priors[i] * multivariate_normal.pdf(x, mean=m, cov=c, allow_singular=True)
-            for i, (m, c) in enumerate(zip(self.means, self.covs))] )
-        norm = np.sum(_, axis=0)
-        _ = np.divide(_, norm)
-        return _.T
-
     def train(self, x, target):
         self.priors = [sum([1.0 for t in target if t==k])/len(target) for k in range(self.num_classes)]
         self.means = [ np.mean( [v for v, t in zip(x, target) if t==k], axis=0 ) for k in range(self.num_classes) ]
