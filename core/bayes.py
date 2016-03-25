fix stddev to variance

core/bayes.py
@@ -22,7 +22,7 @@ class NaivBayes(object):
     def train(self, x, target):
         self.priors = [sum([1.0 for t in target if t==k])/len(target) for k in range(self.num_classes)]
         self.means = [ np.mean( [v for v, t in zip(x, target) if t==k], axis=0 ) for k in range(self.num_classes) ]
-        self.covs = [ np.std( [v for v, t in zip(x, target) if t==k], axis=0 ) for k in range(self.num_classes) ]
+        self.covs = [ np.var( [v for v, t in zip(x, target) if t==k], axis=0 ) for k in range(self.num_classes) ]
 
     def dump_params(self):
         odict = {}
@@ -46,7 +46,7 @@ class NaivBayes(object):
         ...                         [5, 4,  1, 2, 2, 1, 1, 2]]).T,
         ...     target = np.array(  [0, 0,  1, 1, 1, 1, 1, 1] ) )
         >>> str(c)
-        'num_classes:2, priors:[0.25, 0.75], means:[array([ 1.5,  4.5]), array([ 6. ,  1.5])], covs:[array([ 0.5,  0.5]), array([ 1. ,  0.5])]'
+        'num_classes:2, priors:[0.25, 0.75], means:[array([ 1.5,  4.5]), array([ 6. ,  1.5])], covs:[array([ 0.25,  0.25]), array([ 1.  ,  0.25])]'
         >>> y = c.predict( np.array( [[1, 6, 3],
         ...                           [5, 1, 4]] ).T )
         >>> print ['%.2f'%_ for _ in y[0]]